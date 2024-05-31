f89d85c2bc1d81ff1094f41adda505b3674f0a03
MohiuddinPrantiq <u1904103@student.cuet.ac.bd>
2024-05-31 20:57:31 +0600

    1904103

--- src/Bank.java
+++ src/Bank.java
@@ -1,60 +1,54 @@
-
 public class Bank {
-    String accountNumber;
-
-    OperationsQueue operationsQueue;
-
-    int balance = 0;
+    private final String accountNumber;
+    private final OperationsQueue operationsQueue;
+    private int balance = 0;
 
     public Bank(String accountNumber, OperationsQueue operationsQueue) {
         this.accountNumber = accountNumber;
         this.operationsQueue = operationsQueue;
     }
 
-    // A deposit function that will run in parallel on a separate thread. It will be a loop where in each iteration, it read the amount from the operationQueue and deposit the amount.
+    // A deposit function that will run in parallel on a separate thread.
+    // It will be a loop where in each iteration, it reads the amount from the operationsQueue and deposits the amount.
     public void deposit() {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         while (true) {
             int amount = operationsQueue.getNextItem();
-            if(amount == -9999) {
-                break;
+            if (amount == -9999) {
+                break;  // Terminate the loop if the sentinel value is encountered
             }
-            if (amount>0) {
-                balance =  balance + amount;
-                System.out.println("Deposited: " + amount + " Balance: " + balance);
+            synchronized (this) {  // Critical section for updating balance
+                if (amount > 0) {
+                    balance += amount;
+                    System.out.println("Deposited: " + amount + " Balance: " + balance);
+                } else {
+                    operationsQueue.add(amount);
+                    System.out.println("Operation added back: " + amount);
+                }
             }
-            else{
-                operationsQueue.add(amount);
-                System.out.println("operation added back "+amount);
-            }
-
         }
     }
 
-    // A withdraw function that will run in parallel on a separate thread. It will be a loop where in each iteration, it read the amount from the operationQueue and withdraw the amount.
+    // A withdraw function that will run in parallel on a separate thread.
+    // It will be a loop where in each iteration, it reads the amount from the operationsQueue and withdraws the amount.
     public void withdraw() {
         while (true) {
             int amount = operationsQueue.getNextItem();
-            if(amount == -9999) {
-                break;
+            if (amount == -9999) {
+                break;  // Terminate the loop if the sentinel value is encountered
             }
-
-            if(balance+amount<0){
-
-                System.out.println("Not enough balance to deposite "+amount);
-                continue;
+            synchronized (this) {  // Critical section for updating balance
+                if (balance + amount < 0) {
+                    System.out.println("Not enough balance to withdraw: " + amount);
+                    continue;  // Skip to the next iteration if there is not enough balance
+                }
+                if (amount < 0) {
+                    balance += amount;
+                    System.out.println("Withdrawn: " + amount + " Balance: " + balance);
+                } else {
+                    operationsQueue.add(amount);
+                    System.out.println("Operation added back: " + amount);
+                }
             }
-
-            if (amount<0) {
-                balance =  balance + amount;
-                System.out.println("Withdrawn: " + amount + " Balance: " + balance);
-            }
-            else{
-                operationsQueue.add(amount);
-                System.out.println("operation added back "+amount);
-            }
-
         }
     }
 }
